1f8ab9ca86d29372b67982a27600d02763a5d0a3
NOlbert <i17026@hb.dhbw-stuttgart.de>
2019-04-29 10:57:45 +0200

    Added Bytecodeloader + Generaltests

---
+++ src/test/java/General/BytecodeLoader.java
@@ -0,0 +1,27 @@
+package General;
+
+import java.lang.reflect.Method;
+
+/**
+ * Custom Classloader which loads the given byte array.
+ */
+public class BytecodeLoader extends ClassLoader
+{
+    private byte[] byteCode;
+
+    public BytecodeLoader(byte[] byteCode)
+    {
+        this.byteCode = byteCode;
+    }
+
+    @Override
+    public Class findClass(String name)
+    {
+        return defineClass(name, byteCode, 0, byteCode.length);
+    }
+
+    public Method getMethod(String className, String method, Class<?>... parameterTypes) throws NoSuchMethodException
+    {
+        return findClass(className).getMethod(method, parameterTypes);
+    }
+}
