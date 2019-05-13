276c38958bf8abdc1957bf859e3e9188e6588cc1
JulianZepf <i17046@hb.dhbw-stuttgart.de>
2019-05-13 09:29:15 +0200

    Typechecker für überladene Methoden angepasst

--- src/main/java/tastgenerator/ClassObject.java
+++ src/main/java/tastgenerator/ClassObject.java
@@ -3,12 +3,13 @@ package tastgenerator;
 import common.ObjectType;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class ClassObject {
     private HashMap<String, ObjectType> fields;
-    private HashMap<String, Method> methods;
+    private HashMap<String, List<Method>> methods;
 
-    public ClassObject(HashMap<String, ObjectType> fields, HashMap<String, Method> methods) {
+    public ClassObject(HashMap<String, ObjectType> fields, HashMap<String, List<Method>> methods) {
         this.fields = fields;
         this.methods = methods;
     }
@@ -17,7 +18,7 @@ public class ClassObject {
         return fields;
     }
 
-    public HashMap<String, Method> getMethods() {
+    public HashMap<String, List<Method>> getMethods() {
         return methods;
     }
 
