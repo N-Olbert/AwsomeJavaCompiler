cadc049c9ecff12ed1c54f6769f8b6a2b4533bd6
NOlbert <i17026@hb.dhbw-stuttgart.de>
2019-05-13 11:10:15 +0200

    Es gibt kein ++/-- als Unary mehr. Stattdessen Assign + Binary. Vereinfacht Parsetree, sonst ggf.

--- src/test/java/TypedASTTests/TypedInstVarTests.java
+++ src/test/java/TypedASTTests/TypedInstVarTests.java
@@ -2,59 +2,51 @@ package TypedASTTests;
 
 import astgenerator.expressions.*;
 import astgenerator.generalelements.MethodDeclaration;
-import astgenerator.generalelements.UntypedProgram;
 import astgenerator.statements.AssignStatement;
 import astgenerator.statements.Block;
 import common.ObjectType;
-import common.Operators;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 import tastgenerator.TypeCheckerInstance;
 import tastgenerator.expressions.TypedInstVar;
-import tastgenerator.generalelements.TypedProgram;
 import tastgenerator.statements.TypedAssignStatement;
 
 import java.util.ArrayList;
 
 public class TypedInstVarTests
 {
-    private static final String BOOLVAR_NAME = "x1";
-    private static final String INTVAR_NAME = "x2";
-    private static final String CHARVAR_NAME = "x3";
-    private static final String OWNTYPE_NAME = "Test";
-    private static final String OWNTYPEVAR_NAME = "x4";
-
     @Test
     public void testProgramBasicTyping()
     {
-        var program = getBasicProgram();
+        var program = TypedTestClass1.getBasicProgram();
         var converter = new TypeCheckerInstance(program);
         var typedProgram = converter.typeCheck(program);
-        assertBasicProgram(typedProgram);
+        TypedTestClass1.assertBasicProgram(typedProgram);
     }
 
     @Test
     public void testBoolInstVar()
     {
-        testBasicInstVar(BOOLVAR_NAME, ObjectType.BoolType.getName());
+        testBasicInstVar(TypedTestClass1.BOOLVAR_NAME, ObjectType.BoolType.getName());
     }
 
     @Test
     public void testIntInstVar()
     {
-        testBasicInstVar(INTVAR_NAME, ObjectType.IntType.getName());
+        testBasicInstVar(TypedTestClass1.INTVAR_NAME, ObjectType.IntType.getName());
     }
 
     @Test
     public void testCharInstVar()
     {
-        testBasicInstVar(CHARVAR_NAME, ObjectType.CharType.getName());
+        testBasicInstVar(TypedTestClass1.CHARVAR_NAME, ObjectType.CharType.getName());
     }
 
     @Test
     public void testOwnTypeInstVar()
     {
-        testBasicInstVar(OWNTYPEVAR_NAME, OWNTYPE_NAME);
+        testBasicInstVar(TypedTestClass1.OWNTYPEVAR_NAME, TypedTestClass1.OWNTYPE_NAME);
     }
 
     @Test(expected = Exception.class)
@@ -66,18 +58,18 @@ public class TypedInstVarTests
     @Test(expected = Exception.class)
     public void testInstVarBasicFail2()
     {
-        testBasicInstVar(BOOLVAR_NAME, ObjectType.IntType.getName());
+        testBasicInstVar(TypedTestClass1.BOOLVAR_NAME, ObjectType.IntType.getName());
     }
 
     @Test(expected = Exception.class)
     public void testInstVarBasicFail3()
     {
-        testBasicInstVar(OWNTYPEVAR_NAME, ObjectType.JObjectType.getName());
+        testBasicInstVar(TypedTestClass1.OWNTYPEVAR_NAME, ObjectType.JObjectType.getName());
     }
 
     private static void testBasicInstVar(String instVarName, String instVarType)
     {
-        var program = getBasicProgram();
+        var program = TypedTestClass1.getBasicProgram();
         var method =
                 new MethodDeclaration(ObjectType.VoidType, "a", new ArrayList<>(),
                                       new Block(
@@ -98,34 +90,5 @@ public class TypedInstVarTests
         assertEquals(localVar.getObjectType(), ObjectType.getType(instVarName));
     }
 
-    private static UntypedProgram getBasicProgram()
-    {
-        var fieldParams = new ArrayList<String[]>();
-        fieldParams.add(new String[] { ObjectType.BoolType.getName(), BOOLVAR_NAME });
-        fieldParams.add(new String[] { ObjectType.IntType.getName(), INTVAR_NAME });
-        fieldParams.add(new String[] { ObjectType.CharType.getName(), CHARVAR_NAME });
-        fieldParams.add(new String[] { ObjectType.getType(OWNTYPE_NAME).getName(), OWNTYPEVAR_NAME });
-        var fields = UntypedProgramGenerator.getFields(fieldParams);
-        return UntypedProgramGenerator.getProgram(OWNTYPE_NAME, fields, new ArrayList<>());
-    }
 
-    private static void assertBasicProgram(TypedProgram program)
-    {
-        assertNotNull(program);
-        assertNotNull(program.getClasses());
-        assertEquals(program.getClasses().size(), 1);
-        var myclass = program.getClasses().get(0);
-        assertNotNull(myclass);
-        assertEquals(myclass.getClassType(), ObjectType.getType(OWNTYPE_NAME));
-        assertNotNull(myclass.getFields());
-        assertEquals(myclass.getFields().size(), 4);
-        var field = myclass.getFields().stream().filter(x -> BOOLVAR_NAME.equals(x.getName())).findFirst().get();
-        assertEquals(field.getObjectType(), ObjectType.BoolType);
-        field = myclass.getFields().stream().filter(x -> INTVAR_NAME.equals(x.getName())).findFirst().get();
-        assertEquals(field.getObjectType(), ObjectType.IntType);
-        field = myclass.getFields().stream().filter(x -> CHARVAR_NAME.equals(x.getName())).findFirst().get();
-        assertEquals(field.getObjectType(), ObjectType.CharType);
-        field = myclass.getFields().stream().filter(x -> OWNTYPEVAR_NAME.equals(x.getName())).findFirst().get();
-        assertEquals(field.getObjectType(), ObjectType.getType(OWNTYPE_NAME));
-    }
 }
