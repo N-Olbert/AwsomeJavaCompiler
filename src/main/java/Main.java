3f005a0d295f60a0ec6ecbcaea56678221cf2ba9
NOlbert <i17026@hb.dhbw-stuttgart.de>
2019-05-13 12:32:00 +0200

    Added Main + Basic TypedAssignTests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,32 @@
+import common.Global;
+
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Main.main");
+    public static void main(String[] args) throws IOException
+    {
+        for(var inputFileName : args)
+        {
+            try (var file = new FileInputStream(inputFileName))
+            {
+                System.out.println("Starting generation of ast.");
+                var ast = Global.getFactory().getASTGenerator().getAST(file);
+                System.out.println("Starting generation of typed ast.");
+                var typedAst = Global.getFactory().getTypedAstGenerator().getTypedProgram(ast);
+                System.out.println("Generating byte code.");
+                var byteCode = Global.getFactory().getBytecodeGenerator().generate(typedAst);
+                System.out.println("Starting generation of typed ast.");
+                var i = 0;
+                for (var m : byteCode)
+                {
+                    var name = typedAst.getClasses().get(i++).getClassType().getName();
+                    try (var os = new FileOutputStream(name, false))
+                    {
+                        os.write(m.toByteArray());
+                    }
+                }
+            }
+        }
     }
 }
