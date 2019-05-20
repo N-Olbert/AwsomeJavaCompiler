bca6ba4f5aa07dac0f898fce9c6a17dcf5ad46d8
sticktor <timroethel@web.de>
2019-05-20 12:14:11 +0200

    Fibonacci with non primitive int

---
+++ src/test/resources/FibonacciWithBetterInt.java
@@ -0,0 +1,14 @@
+public class FibonacciWithBetterInt
+{
+    public BetterInt fibonacci(BetterInt n)
+    {
+        if (n.LessThan(new BetterInt(2)))
+        {
+            return n;
+        }
+        else
+        {
+            return fibonacci(n.Subtract(new BetterInt(1))).Add(fibonacci(n.Subtract(new BetterInt(2))));
+        }
+    }
+}
