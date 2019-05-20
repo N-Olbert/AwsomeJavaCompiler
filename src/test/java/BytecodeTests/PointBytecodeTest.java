2ae7a54ebf4bb7623c5cbcc83bf6457e8ab3e1cf
Nico Dreher <Nico.Dreher98@gmail.com>
2019-05-20 11:08:24 +0200

    Fixed Point

--- src/test/java/BytecodeTests/PointBytecodeTest.java
+++ src/test/java/BytecodeTests/PointBytecodeTest.java
@@ -10,15 +10,13 @@ import org.objectweb.asm.ClassWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static BytecodeTests.Point.*;
+import static org.junit.Assert.*;
 
 
-public class PointBytecodeTest
-{
+public class PointBytecodeTest {
     @Test
-    public void testPoint()
-    {
+    public void testPoint() {
         Factory factory = Global.getFactory();
         assertNotNull(factory);
         BytecodeGenerator byteCodeGen = factory.getBytecodeGenerator();
@@ -30,75 +28,64 @@ public class PointBytecodeTest
         assertNotNull(cws);
         assertEquals(cws.size(), 1);
         byte[] bytes = cws.get(0).toByteArray();
-
+        BytecodeTests.saveClass(bytes, "target/point.class");
         BytecodeLoader loader = new BytecodeLoader(bytes);
         var Class = loader.findClass(className);
         assertNotNull(Class);
         Object classObject = null;
-        try
-        {
+        try {
             var constructor = loader.getConstructor(className, int.class, int.class);
             assertEquals(className, constructor.getDeclaringClass().getName());
             assertNotNull(classObject = constructor.newInstance(3, 9));
         }
-        catch (NoSuchMethodException e)
-        {
+        catch(NoSuchMethodException e) {
             fail("Constructor: \"" + constructorName + "\" not found");
         }
-        catch (InstantiationException | IllegalAccessException | InvocationTargetException e)
-        {
-            fail("Invoking constructor: \""+ constructorName + " \"");
+        catch(InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            fail("Invoking constructor: \"" + constructorName + " \"");
         }
 
-        try
-        {
-            var method = loader.getMethod(className, methodNameGetX, int.class);
+        try {
+            var method = loader.getMethod(className, methodNameGetX);
             assertEquals(int.class, method.getReturnType());
             assertEquals(3, method.invoke(classObject));
         }
-        catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e)
-        {
-            fail(methodNameGetX+" failed");
+        catch(IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            fail(methodNameGetX + " failed");
         }
 
-        try
-        {
-            var method = loader.getMethod(className, methodNameGetY, int.class);
+        try {
+            var method = loader.getMethod(className, methodNameGetY);
             assertEquals(int.class, method.getReturnType());
             assertEquals(9, method.invoke(classObject));
         }
-        catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e)
-        {
-            fail(methodNameGetY+" failed");
+        catch(IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            fail(methodNameGetY + " failed");
         }
 
-        try
-        {
+        try {
             var method = loader.getMethod(className, methodNameSetX, int.class);
             assertEquals(void.class, method.getReturnType());
             method.invoke(classObject, 7);
 
-            var methodGet = loader.getMethod(className, methodNameGetX, int.class);
+            var methodGet = loader.getMethod(className, methodNameGetX);
             assertEquals(int.class, methodGet.getReturnType());
-            assertEquals(7, method.invoke(classObject));
+            assertEquals(7, methodGet.invoke(classObject));
         }
-        catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e)
-        {
-            fail(methodNameSetX+" failed");
+        catch(IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            fail(methodNameSetX + " failed");
         }
 
-        try
-        {
+        try {
             var method = loader.getMethod(className, methodNameSetY, int.class);
             assertEquals(void.class, method.getReturnType());
             method.invoke(classObject, 4);
-            var methodGet = loader.getMethod(className, methodNameGetY, int.class);
+            var methodGet = loader.getMethod(className, methodNameGetY);
             assertEquals(int.class, methodGet.getReturnType());
             assertEquals(4, methodGet.invoke(classObject));
         }
-        catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e)
-        {
-            fail(methodNameSetY+" failed");
+        catch(IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            fail(methodNameSetY + " failed");
         }
     }
 }
