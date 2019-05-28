eba7e364a9c858a5346b794280ac26e880de1b6a
NOlbert <i17026@hb.dhbw-stuttgart.de>
2019-05-28 22:19:15 +0200

    Fix: Im zweiten ctor LocalOrFieldVar statt InstVar
    Co-Authored-By: danielbornbaum <danielbornbaum@users.noreply.github.com>

--- src/test/java/General/BetterInt.java
+++ src/test/java/General/BetterInt.java
@@ -33,7 +33,7 @@ public class BetterInt
 
         var ctor2 = new MethodDeclaration(AccessModifier.PUBLIC, Modifier.NONE, ObjectType.VoidType, "BetterInt",
                 new ArrayList<>(),
-                new Block(new AssignStatement(new InstVar(new This(), "x"), new JInteger("0"))));
+                new Block(new AssignStatement(new LocalOrFieldVar("x"), new JInteger("0"))));
 
         List<Expression> args = new ArrayList<>();
         args.add(
@@ -94,7 +94,7 @@ public class BetterInt
         var ctor2 = new TypedMethodDeclaration(AccessModifier.PUBLIC, Modifier.NONE, ObjectType.VoidType, "BetterInt",
                 new ArrayList<>(),
                 new TypedBlock(ObjectType.VoidType,
-                               new TypedAssignStatement(new TypedInstVar(new TypedThis(classType), "x", ObjectType.IntType), new TypedInt("0"))));
+                               new TypedAssignStatement(new TypedLocalOrFieldVar(ObjectType.IntType, "x"), new TypedInt("0"))));
 
         List<TypedExpression> args = new ArrayList<>();
         args.add(
